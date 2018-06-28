c152fbe896cfe14b1ad49e9c607e5a4b65619085
Vladimir Oltean <olteanv@gmail.com>
2018-06-28 13:03:37 +0300

    YTX703: remove CNEService app
    * Role according to Qualcomm:
      https://www.qualcomm.com/news/onq/2013/07/02/qualcomms-cne-bringing-smarts-3g4g-wi-fi-seamless-interworking
    * When a SIM card is inserted into the YTX703L, the library fails to
      call Looper.init() in the telephony stack (log below). Figuring out
      why is not in top 100 priorities right now, so we are simply removing
      the service.
    
    FATAL EXCEPTION: MainEventThread
    Process: .dataservices, PID: 2422
    java.lang.NoSuchMethodError: No direct method <init>(ILandroid/os/Looper;)V in
      class Landroid/telephony/PhoneStateListener; or its super classes (declaration
      of '/system/framework/framework.jar!classes2.dex)
           at com.quicinc.cne.CNE$8.<init>(CNE.java:2477)
           at com.quicinc.cne.CNE.registerPhoneStateListener(CNE.java:2477)
           at com.quicinc.cne.CNE.-wrap5(Unknown Source:0)
           at com.quicinc.cne.CNE$6.handleMessage(CNE.java:1681)
           at android.os.Handler.dispatchMessage(Handler.java:106)
           at android.os.Looper.loop(Looper.java:164)
           at android.os.HandlerThread.run(HandlerThread.java:65)
    
    Change-Id: I74a39f81bfc57caa17db8aa626a4857240ba44c5
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- YTX703-common/YTX703-common-vendor.mk
+++ YTX703-common/YTX703-common-vendor.mk
@@ -963,7 +963,6 @@ PRODUCT_PACKAGES += \
     libtime_genoff \
     libts_detected_face_hal \
     libts_face_beautify_hal \
-    CNEService \
     qcrilmsgtunnel \
     com.qualcomm.location \
     ConnectivityExt \
