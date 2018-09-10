4e943663dbabbc25ebdebbfbb02ef3fc8db63363
Quallenauge <Hamsi2k@freenet.de>
2018-09-10 01:55:01 -0100

    YTX703: Remove duplicate vendor.qti.gnss@1.0 service definition
    * There's no need to fetch this from the vendor firmware, since we
      already provide this in the device tree under configs/init/.
    
    Change-Id: Ia351761a93a7a69695ec47ce25c6410e1c9c18b1

--- YTX703-common/YTX703-common-vendor.mk
+++ YTX703-common/YTX703-common-vendor.mk
@@ -145,7 +145,6 @@ PRODUCT_COPY_FILES += \
     vendor/lenovo/YTX703-common/proprietary/vendor/bin/xtwifi-client:$(TARGET_COPY_OUT_VENDOR)/bin/xtwifi-client \
     vendor/lenovo/YTX703-common/proprietary/vendor/bin/xtwifi-inet-agent:$(TARGET_COPY_OUT_VENDOR)/bin/xtwifi-inet-agent \
     vendor/lenovo/YTX703-common/proprietary/vendor/etc/cacert_location.pem:$(TARGET_COPY_OUT_VENDOR)/etc/cacert_location.pem \
-    vendor/lenovo/YTX703-common/proprietary/vendor/etc/init/vendor.qti.gnss@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.gnss@1.0-service.rc \
     vendor/lenovo/YTX703-common/proprietary/vendor/etc/init/vendor.qti.hardware.perf@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.perf@1.0-service.rc \
     vendor/lenovo/YTX703-common/proprietary/vendor/etc/xtra_root_cert.pem:$(TARGET_COPY_OUT_VENDOR)/etc/xtra_root_cert.pem \
     vendor/lenovo/YTX703-common/proprietary/vendor/lib/egl/eglSubDriverAndroid.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/eglSubDriverAndroid.so \
