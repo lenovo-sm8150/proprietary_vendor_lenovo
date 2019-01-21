413a9a8c1690227154694af756dc158a09906449
Quallenauge <Hamsi2k@freenet.de>
2019-01-22 01:24:49 +0200

    YTX703: Update blobs from bq bardock pro firmware 2.5.1
    Change-Id: I571b5e09ed6aab10a86cf701d311cb791a39cf2b

--- YTX703-common/Android.mk
+++ YTX703-common/Android.mk
@@ -115,14 +115,13 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
 LOCAL_MODULE_OWNER := lenovo
-LOCAL_SRC_FILES := proprietary/vendor/priv-app/com.qualcomm.location/com.qualcomm.location.apk
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
