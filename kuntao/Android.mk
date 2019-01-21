1223e02228f3423d4951ff4594a47e79e7e1fe9b
Michael Bestas <mkbestas@lineageos.org>
2019-01-21 17:38:56 +0200

    kuntao: Kill HIDL ANT+ support
    CAF removed it for 8953 in P. Furthermore, I'm not sure if it ever worked for us.
    
    Change-Id: Idf9a35f68efa44834129ecd5452850301640626c

--- kuntao/Android.mk
+++ kuntao/Android.mk
@@ -19,17 +19,6 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),kuntao)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libantradio
-LOCAL_MODULE_OWNER := lenovo
-LOCAL_SRC_FILES_64 := proprietary/lib64/libantradio.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libantradio.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libgpustats
 LOCAL_MODULE_OWNER := lenovo
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libgpustats.so
